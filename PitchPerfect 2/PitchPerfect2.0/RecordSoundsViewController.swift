da2831c7c6cae8254993f476ad67aa8386ad1f41
rrballenger17 <rrb@alumni.stanford.edu>
2016-05-15 23:52:23 -0400

    pitchperfectnew

---
+++ PitchPerfect 2/PitchPerfect2.0/RecordSoundsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  PitchPerfect2.0
+//
+//  Created by Ryan Ballenger on 5/15/16.
+//  Copyright © 2016 Ryan Ballenger. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    var audioRecorder:AVAudioRecorder!
+
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in progress"
+    
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+        
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        
+        audioRecorder.delegate = self
+        
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop recording button pressed")
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        recordingLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+
+    }
+
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool){
+        print("AVAudioRecorder finished saving recording")
+        self.performSegueWithIdentifier("stopRecording", sender: audioRecorder.url)
+    }
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "stopRecording") {
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
+}
+
---
+++ PitchPerfect 2/README.md
@@ -0,0 +1,3 @@
+Iphone application that records and plays back your voice with sound effects. 
+
+Created while completing Udacity's Intro to iOS App Development with Swift.
